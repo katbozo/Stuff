a0d8a21ef6b50538eac2fb8ee96b4ba8f8ee806c
katbozo <142151193+katbozo@users.noreply.github.com>
2023-08-21 20:16:52 +0400

    Update KatHub.lua

--- KatHub.lua
+++ KatHub.lua
@@ -2,4 +2,6 @@ local CurrGame = game.GameId
 
 if CurrGame == 2217006564 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/katbozo/Stuff/main/OBFUSCATEDAMT1.lua'))()
+elseif CurrGame == 2533050278 then
+	--RMA
 end
