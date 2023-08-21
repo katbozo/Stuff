2c5cbf575234bc0bbd67d0fb3359a3b3f7613c89
katbozo <142151193+katbozo@users.noreply.github.com>
2023-08-21 20:38:49 +0400

    Update KatHub.lua

--- KatHub.lua
+++ KatHub.lua
@@ -1,7 +1,7 @@
 local CurrGame = game.GameId
 
 if CurrGame == 2217006564 then
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/katbozo/Stuff/main/OBFUSCATEDAMT1.lua'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/katbozo/Stuff/main/AMenacingTimeline.lua'))()
 elseif CurrGame == 2533050278 then
-	--RMA
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/katbozo/Stuff/main/RateMyAvatar.lua'))()
 end
